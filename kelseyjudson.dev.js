651c1fe15e6b4988917574f3cea4273660a3d38b
Kelsey Judson <k+github@judson.nz>
2020-09-28 22:50:19 -0500

    Click anywhere to close dropdowns

--- kelseyjudson.dev.js
+++ kelseyjudson.dev.js
@@ -1,9 +1,15 @@
-// Dropdown menu
+// Dropdown menus
 
 document.querySelector('.has-dropdown').addEventListener('click', event => {
   if (event.target.closest('.dropdown') === null) {
     event.preventDefault();
     event.currentTarget.classList.toggle('is-open');
+  }
+});
+
+document.documentElement.addEventListener('click', event => {
+  if (event.target.closest('.has-dropdown') === null) {
+    document.querySelectorAll('.has-dropdown').forEach(element => element.classList.remove('is-open'));
   }
 });
 
