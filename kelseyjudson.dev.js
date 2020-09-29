376978d6653aa4d5b440a941455e379ce3e2a89f
Kelsey Judson <k+github@judson.nz>
2020-09-28 22:46:50 -0500

    Dropdown menu

--- kelseyjudson.dev.js
+++ kelseyjudson.dev.js
@@ -1,3 +1,14 @@
+// Dropdown menu
+
+document.querySelector('.has-dropdown').addEventListener('click', event => {
+  if (event.target.closest('.dropdown') === null) {
+    event.preventDefault();
+    event.currentTarget.classList.toggle('is-open');
+  }
+});
+
+// Scroll
+
 function setIsScrolled() {
   if (window.pageYOffset === 0) {
     document.documentElement.classList.remove('is-scrolled');
